6be7407402853889632a1f7a774549dcb27dad41
Juan in a million <59286923+damianjuan@users.noreply.github.com>
2020-04-09 02:44:40 -0500

    created functions to store user score and initials

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 var end = false;
+var scoreList = document.querySelector(".score-list");
+var userInput = document.querySelector("#initials");
+
+
+var users = [];
+var userScores = [];
+
+
+
+function renderScores() {
+    for (var i = 0; i < users.length; i++) {
+        var user = users[i];
+        var score = userScores[i];
+        var li = document.createElement("li");
+        li.textContent = user;
+        scoreList.appendChild(li);
+    };
+};
+
+
+function storeUserAndScore() {
+    localStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("userScores", JSON.stringify(userScores));
+}
 
 //listen for click to start quiz
 document.getElementById("startBtn").addEventListener('click', function () {
@@ -19,23 +43,23 @@ document.getElementById("question1Btns").addEventListener('click', function () {
 
 });
 
-document.getElementById("question2").addEventListener('click', function () {
+document.getElementById("question2Btns").addEventListener('click', function () {
     document.getElementById("question2").setAttribute("style", "display: none");
     document.getElementById("question3").setAttribute("style", "display: show");
 
 });
 
-document.getElementById("question3").addEventListener('click', function () {
+document.getElementById("question3Btns").addEventListener('click', function () {
     document.getElementById("question3").setAttribute("style", "display: none");
     document.getElementById("question4").setAttribute("style", "display: show");
 });
 
-document.getElementById("question4").addEventListener('click', function () {
+document.getElementById("question4Btns").addEventListener('click', function () {
     document.getElementById("question4").setAttribute("style", "display: none");
     document.getElementById("question5").setAttribute("style", "display: show");
 });
 
-document.getElementById("question5").addEventListener('click', function () {
+document.getElementById("question5Btns").addEventListener('click', function () {
     document.getElementById("question5").setAttribute("style", "display: none");
     document.getElementById("quizOver").setAttribute("style", "display: show");
     document.getElementById("timer").setAttribute("style", "display: none");
@@ -49,6 +73,24 @@ document.getElementById("submitInitials").addEventListener('click', function () 
     document.getElementById("initials");
     document.getElementById("quizOver").setAttribute("style", "display: none");
     document.getElementById("scores").setAttribute("style", "display: show");
+
+    var userText = userInput.value.trim();
+    var scoreEntry = seconds;
+    if (userText === "") {
+        return;
+    }
+
+    users.push(userText);
+    userInput.value = "";
+
+    userScores.push(scoreEntry);
+    userInput.value = "";
+
+
+    storeUserAndScore();
+    renderScores();
+
+
 });
 
 document.getElementById("retry").addEventListener('click', function () {
